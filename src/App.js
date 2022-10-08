11ac4f87cfc4c675691dccc58bf8a2fba3bf116b
Vlad Silagy <vladsilagy@gmail.com>
2022-10-08 14:33:50 -0400

    add create-react-app to development branch

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Chingus!</h1>
+      <p>V41-toucans-team-04</p>
+    </div>
+  );
+}
+
+export default App;
