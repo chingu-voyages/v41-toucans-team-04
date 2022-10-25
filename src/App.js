5801c5af1d2787e7a8984db1152eb69b29a02883
Milo <miloprince92@gmail.com>
2022-10-25 12:02:23 -0400

    Assets and routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import Menu from "./components/Menu";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import "./fonts/Atkinson-Hyperlegible-Regular-102.ttf";
+import "./fonts/Atkinson-Hyperlegible-Bold-102.ttf";
+import "./fonts/Atkinson-Hyperlegible-BoldItalic-102.ttf";
+import "./fonts/Atkinson-Hyperlegible-Italic-102.ttf";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Menu />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/menu" element={<Menu />} />
+
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/shopping" element={<Shop />} /> */}
+          {/* Components not made yet */}
+
+          <Route path="*" element={<Navigate to="/" />} />
+
+          {/* Wildcard - if a route is entered in URL with no set path, it will redirect to Main */}
+        </Routes>
+      </Router>
     </div>
   );
 }
