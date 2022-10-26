8d59c742dd2bf9f0479cacbfbdb6338e1c3f7cf0
jwzzang12 <moonbb12@gmail.com>
2022-10-26 17:56:22 +0900

    Set data.json (coffee/non-coffee)

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,12 @@
 import { useEffect, useState } from "react";
-// import data from "../assets/data.json";
-import axios from "axios";
+import data from "../assets/data.json";
 import Card from "./Card";
 import "./css/Menu.css";
 
 export default function Menu() {
   const [card, setCard] = useState([]);
-  // useEffect(() => {
-  //   setCard(data.coffees);
-  // }, []);
   useEffect(() => {
-    axios.get(`https://api.sampleapis.com/coffee/hot`).then((res) => {
-      setCard(res.data);
-    });
+    setCard(data.coffee);
   }, []);
   return (
     <section id="menu">
