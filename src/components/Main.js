7017732b45160ab0344bafe93938f8265a5be16c
vsilagy <vladsilagy@gmail.com>
2022-10-27 15:30:51 -0400

    add max-width to Header and Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,17 +2,19 @@ import './Main.css';
 
 const Main = () => {
 	return (
-		<div className="main">
-			<h1 className="main-title">
-				Great Coffee <br /> made simple.
-			</h1>
-			<p className="main-text">
-				Start your mornings with the world’s best coffees. Try our
-				expertly curated artisan coffees from our best roasters
-				delivered directly to your door, at your schedule.
-			</p>
-			<button className="main-btn">Create your Plan</button>
-		</div>
+		<section className="main">
+			<div className="main-wrapper">
+				<h1 className="main-title">
+					Great Coffee <br /> made simple.
+				</h1>
+				<p className="main-text">
+					Start your mornings with the world’s best coffees. Try our
+					expertly curated artisan coffees from our best roasters
+					delivered directly to your door, at your schedule.
+				</p>
+				<button className="main-btn">Create your Plan</button>
+			</div>
+		</section>
 	);
 };
 
