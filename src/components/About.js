98d9d3ffc1cc434d96fb9b40e1a4705907135dd1
vsilagy <vladsilagy@gmail.com>
2022-10-29 00:56:39 -0400

    add About and Plan pages

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import './About.css';
+import video from '../assets/video-filter-cup.mp4';
+import poster from '../assets/image-hero-whitecup.jpg';
+const About = () => {
+	return (
+		<>
+			<section className="about">
+				<div className="main-container container">
+					<h1 className="main-title">About Us</h1>
+					<p className="main-text">
+						Coffeeroasters began its journey of exotic discovery in
+						1999, highlighting stories of coffee from around the
+						world. We have since been dedicated to bring the perfect
+						cup - from bean to brew - in every shipment.
+					</p>
+					<video
+						src={video}
+						poster={poster}
+						autoPlay
+						loop
+						muted
+						crossOrigin="true"
+						className="main-video"></video>
+					<div className="overlay"></div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default About;
---
+++ src/components/Plan.js
@@ -0,0 +1,12 @@
+import './Plan.css';
+const Plan = () => {
+	return (
+		<>
+			<section>
+				<h1>Create Your Plan</h1>
+			</section>
+		</>
+	);
+};
+
+export default Plan;
