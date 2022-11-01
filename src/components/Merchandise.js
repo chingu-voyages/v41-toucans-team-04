f6897d24c1ff2f0b68df84650470ad3df75e7517
vsilagy <vladsilagy@gmail.com>
2022-11-01 13:57:50 -0400

    add shop hero, center the content, move card-container outside map function, merch mobile is mobile responsive

--- src/components/Merchandise.js
+++ src/components/Merchandise.js
@@ -1,31 +1,61 @@
-import React from "react";
-import "./Merchandise.css";
-import { useEffect, useState } from "react";
-import data from "../assets/merch-items.json";
+import React from 'react';
+import './Merchandise.css';
+import { useEffect, useState } from 'react';
+import data from '../assets/merch-items.json';
+import video from '../assets/video-filter-cup.mp4';
+import poster from '../assets/image-coffee-shop.jpg';
 
 export default function Merchandise() {
-  const [products, setProducts] = useState(data.merch);
+	const [products, setProducts] = useState(data.merch);
 
-  useEffect(() => {
-    setProducts(products);
-  }, [products]);
-  console.log(products);
-  return (
-    <section className="merch">
-      {products.map((item) => {
-        return (
-          <div className="card-container">
-            <div className="card">
-              {/* <img src={item.img} alt={item.name} /> */}
-              <div className="placeholder"></div>
-              <p className="name">{item.name}</p>
-              <p className="description">{item.description}</p>
-              <p className="price">{item.price}</p>
-              <button>Add to Card</button>
-            </div>
-          </div>
-        );
-      })}
-    </section>
-  );
+	useEffect(() => {
+		setProducts(products);
+	}, [products]);
+	console.log(products);
+	return (
+		<>
+			<section className="merch-hero">
+				<div className="hero-container container">
+					<h1 className="hero-title">
+						{/* add any title */}
+						Love our coffee <br />
+						Enjoy our merch <br />
+					</h1>
+					{/* add any text */}
+					<p className="hero-text">Toucan Coffee shop</p>
+					<video
+						src={video}
+						type="video/mp4"
+						poster={poster}
+						autoPlay
+						loop
+						muted
+						webkit-playsinline
+						playsinline
+						className="hero-video"></video>
+					<div className="hero-overlay"></div>
+				</div>
+			</section>
+			<section className="merch">
+				{/* add this div for layout */}
+				<div className="merch-container container">
+					{products.map((item) => {
+						return (
+							// container should be outside map , will use merch-container
+							// <div className="card-container">
+							<div className="card">
+								{/* <img src={item.img} alt={item.name} /> */}
+								<div className="placeholder"></div>
+								<p className="name">{item.name}</p>
+								<p className="description">{item.description}</p>
+								<p className="price">{item.price}</p>
+								<button>Add to Card</button>
+							</div>
+							// </div>
+						);
+					})}
+				</div>
+			</section>
+		</>
+	);
 }
