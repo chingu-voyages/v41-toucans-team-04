5e23e2005d1add89eebb810af84219a7d3ad860c
vivchc <viannc6284@gmail.com>
2022-11-04 21:30:47 -0400

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-
-
-import Header from './components/Header';
-import Main from './components/Main';
-import Menu from './components/Menu';
-import About from './components/About';
-import Plan from './components/Plan';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Plan from "./components/Plan";
+import Footer from "./components/Footer";
 import {
-	BrowserRouter as Router,
-	Navigate,
-	Route,
-	Routes,
-} from 'react-router-dom';
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/plan" element={<Plan />} />
+          {/*<Route path="/shopping" element={<Shop />} /> */}
+          {/* Components not made yet */}
 
-	return (
-		<div className="App">
-			<Router>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Main />} />
-					<Route path="/menu" element={<Menu />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/plan" element={<Plan />} />
-					{/*<Route path="/shopping" element={<Shop />} /> */}
-					{/* Components not made yet */}
+          <Route path="*" element={<Navigate to="/" />} />
 
-					<Route path="*" element={<Navigate to="/" />} />
-
-					{/* Wildcard - if a route is entered in URL with no set path, it will redirect to Main */}
-				</Routes>
-				<Footer />
-			</Router>
-		</div>
-	);
+          {/* Wildcard - if a route is entered in URL with no set path, it will redirect to Main */}
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
