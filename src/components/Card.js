16e4eec10db3a312ad80682e78e637c9f24da409
Vlad <vladsilagy@gmail.com>
2022-11-12 09:35:57 -0500

    fix layout of the card, add card-btn class, change button radius

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,34 @@
-import { useState } from "react";
-import "./Card.css";
+import { useState } from 'react';
+import './Card.css';
 
 export default function Card({ data, id, addToCart }) {
-  const [flip, setFlip] = useState(true);
-  const onClickFlip = () => {
-    setFlip(!flip);
-  };
+	const [flip, setFlip] = useState(true);
+	const onClickFlip = () => {
+		setFlip(!flip);
+	};
 
-  const addCartHandler = () => {
-    addToCart(id, data.name, data.price);
-  };
+	const addCartHandler = () => {
+		addToCart(id, data.name, data.price);
+	};
 
-  return (
-    <li className="card">
-      {flip ? (
-        <div className="image" onClick={onClickFlip}>
-          <img src={data.image} alt={data.name} />
-        </div>
-      ) : (
-        <p className="desc" onClick={onClickFlip}>
-          <span>{data.description}</span>
-        </p>
-      )}
-      <div className="name">{data.name}</div>
-      <div className="price">${data.price}</div>
-      <button onClick={addCartHandler}>Add to Cart</button>
-    </li>
-  );
+	return (
+		<li className="card">
+			{flip ? (
+				<div className="image" onClick={onClickFlip}>
+					<img src={data.image} alt={data.name} />
+				</div>
+			) : (
+				<p className="desc" onClick={onClickFlip}>
+					<span>{data.description}</span>
+				</p>
+			)}
+			<div className="price-wrapper">
+				<div className="name">{data.name}</div>
+				<div className="price">${data.price}</div>
+			</div>
+			<button className="card-btn" onClick={addCartHandler}>
+				Add to Cart
+			</button>
+		</li>
+	);
 }
